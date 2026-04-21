a32fb54232e080906a4fd1ec1d8a8850dc4d4416
BlackCumin <kostik.kocherga@gmail.com>
2026-04-21 15:59:45 +0300

    Add PostgreSQL config

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -94,9 +94,8 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600
     )
 }
 
