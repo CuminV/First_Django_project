b8e73060019f156837b7d106e1b70bfbc3aea72d
BlackCumin <kostik.kocherga@gmail.com>
2026-04-22 10:07:40 +0300

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -141,3 +141,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #My setings
 LOGIN_URL = '/users/login/'
 
+#Heroku settings
+import django_heroku
+django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
